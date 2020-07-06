8000fb16aae201659b87dc7641a959cb4d70e20c
Jules Prevost <jprevostdev@gmail.com>
2020-07-06 14:46:36 +0200

    models ok

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,3 +1,5 @@
 class Cell < ApplicationRecord
   belongs_to :party
+
+  enum symbol: { none: 0, cross: 1, circle: 2 }, default: :none
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,6 @@
 class Party < ApplicationRecord
   belongs_to :user
   has_many :cells
+
+  enum win: { user: 0, computer: 1, none: 2 }
 end
